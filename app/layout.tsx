e0883c5225d0dce08fc138f8bc019cbeeecb9759
Papp Balázs <hello@pappbalazs.com>
2025-05-16 22:12:13 +0200

    Adding viewport.

--- app/layout.tsx
+++ app/layout.tsx
@@ -240,6 +240,13 @@ export const metadata: Metadata = {
 	manifest: "/manifest.json",
 };
 
+export const viewport: Viewport = {
+	width: "device-width",
+	initialScale: 1,
+	maximumScale: 1,
+	userScalable: false,
+};
+
 export default function RootLayout({
 	children,
 }: {
